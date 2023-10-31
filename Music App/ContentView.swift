a3232a87943be28c0db4460585873ee4f2ea5a9a
Nathan Davis <anathangd@gmail.com>
2023-10-31 08:14:02 -0400

    Initial Commit

---
+++ Music App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Music App
+//
+//  Created by Nathan Davis on 10/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Music App/Music_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_AppApp.swift
+//  Music App
+//
+//  Created by Nathan Davis on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Music_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
